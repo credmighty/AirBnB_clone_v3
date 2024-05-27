e5c91138a127fbb96ce471a9b9befcad6380dc96
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-05-28 00:20:33 +0100

    add pyco

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,22 +2,28 @@
 """the application file"""
 import storage from models
 import app_views from api.v1.views
-from flask import Flask
+from os import environ
+from flask import Flask, render_template, make_response, jsonify
+from flask_cors import CORS
+from flasgger import Swagger
+from flasgger.utils import swag_from
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
-def close_dataB():
-   """close storage"""
-   storage.close()
+def close_db(error):
+    """close storage"""
+    storage.close()
+
 
 if __name__ == "__main__":
-   """main function"""
-   host = environ.get('HBNB_API_HOST')
-   port = environ.get('HBNB_API_PORT')
-   if not host:
-       host = 0.0.0.0
-   if not port:
-       port = '5000'
-   app.run(host=host, port=port, threaded=True)
+    """main function"""
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = 0.0.0.0
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
